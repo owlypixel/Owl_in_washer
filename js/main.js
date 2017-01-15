0d750de1db8653fa5960cce340929427f99c7955
Owlypixel <owlypixel@gmail.com>
2017-01-16 00:02:52 +0200

    added graceful dial animation

--- js/main.js
+++ js/main.js
@@ -1,3 +1,28 @@
-// $(function(){
-	
-// });
+const pwrbtn = document.querySelector(".dial");
+const outer = document.querySelector(".outer");
+
+//gracefully start and stop animation of the dial
+var running = false;
+pwrbtn.addEventListener('click', function(e){
+	this.classList.toggle('active');
+	if(running){
+		stop();
+		running = false;
+	} else {
+		start();
+		running = true;
+	}
+})
+
+function start() {
+	outer.classList.add('active');
+}
+
+function stop() {
+  outer.addEventListener('animationiteration', callback);
+
+  function callback() {
+    outer.classList.remove('active');
+    outer.removeEventListener('animationiteration', callback);
+  }
+}
